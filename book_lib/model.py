36e74e34a97ced6ba06010fba0cab571820d4e37
krzysztofwroniak <cr0wss@outlook.com>
2025-12-01 10:33:29 +0100

    wip: imported old aplication

---
+++ book_lib/model.py
@@ -0,0 +1,7 @@
+users:list=[
+    # {'name': 'Krzysztof', 'location': 'Wołomin', 'posts': 1, 'img_url':''},
+    # {'name': 'Bartek', 'location': 'Warszawa', 'posts': 4, 'img_url':''},
+    # {'name': 'Antek', 'location': 'Berlin', 'posts': 6, 'img_url':''},
+    # {'name': 'Kasia', 'location': 'Olsztyn', 'posts': 8, 'img_url':''},
+
+]
